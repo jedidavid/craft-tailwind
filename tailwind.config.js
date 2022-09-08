861a7ceba4eec76802b3ff125ab452a47c3913ad
jedidavid <jeremydb.david@gmail.com>
2022-09-09 07:26:14 +0800

    craft + tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/**/*.twig", "./templates/**/*.html", "./web/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
---
+++ webpack.mix.js
@@ -0,0 +1,14 @@
+const mix = require("laravel-mix");
+
+mix
+  .js("src/js/index.js", "web/js")
+  .postCss("src/css/app.css", "web/css", [require("tailwindcss")])
+  .browserSync({
+    proxy: "localhost:5555",
+    files: [
+      "templates/**/*.twig",
+      "templates/**/*.html",
+      "web/**/.js",
+      "web/**/.css",
+    ],
+  });
